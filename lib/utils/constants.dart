9c6ce54cbe364bd2e12a3d789723598e33541343
AYOMITIDE OAJ <36337827+AYOMITIDE-OAJ@users.noreply.github.com>
2023-01-16 19:14:11 +0100

    activated the openai module

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: non_constant_identifier_names
 
-String OPEN_API_KEY = 'ENTER_YOUR_API_KEY_HERE';
+String OPEN_API_KEY = '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';
 
 String BASE_URL = 'https://api.openai.com/v1/';
-String ANDROID_INTERSTITIAL_ADD_ID = 'ca-app-pub-3940256099942544/1033173712';
-String IOS_INTERSTITIAL_ADD_ID = 'ca-app-pub-3940256099942544/4411468910';
+String ANDROID_INTERSTITIAL_ADD_ID = '';
+String IOS_INTERSTITIAL_ADD_ID = '';
 
-String ANDROID_BANNER_ADD_ID = 'ca-app-pub-3940256099942544/6300978111';
-String IOS_BANNER_ADD_ID = 'ca-app-pub-3940256099942544/6300978111';
+String ANDROID_BANNER_ADD_ID = '';
+String IOS_BANNER_ADD_ID = '';
